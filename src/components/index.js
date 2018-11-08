b3228a592b48b8ea43e0367ad48731c3fef43254
Alihan Berk Bilgiç <alihanberkbilgic@Alihans-MacBook-Pro.local>
2018-11-08 23:45:38 +0300

    Creating

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Header } from './Views'
+
+class Page extends Component {
+
+    render() {
+        return (
+            <Header />
+        );
+    }
+}
+
+export default Page
